5ba1ad0843313332707f3f663223eef1f177d565
adilett02 <adiletturdakunov2002@gmail.com>
2021-03-11 15:17:51 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -4,16 +4,6 @@
 
     "use strict";
     
-    // loader
-    var loader = function () {
-        setTimeout(function () {
-            if ($('#loader').length > 0) {
-                $('#loader').removeClass('show');
-            }
-        }, 1);
-    };
-    loader();
-    
     // Initiate the wowjs
     new WOW().init();
     
